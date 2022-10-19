a0b6b168086daceb02bf6f81875fcc0b592d30a7
korosa péter <97808318+Korosapeter@users.noreply.github.com>
2022-10-19 18:17:11 +0200

    Add files via upload

---
+++ CSibész tömb/madarnyelv_visszaalakitas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace madarnyelv_visszaalakitas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hatos = 0;
+            bool voltmar = false;
+            byte[] dobások = new byte[100];
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                dobások[i] = (byte)random.Next(0, 6);
+                if (dobások[i] == 6)
+                {
+                    hatos++;
+                }
+                if (dobások[i] == 6 && voltmar == false)
+                {
+                    Console.WriteLine("első hatos{i+1}");
+                    voltmar = true;
+                }
+            }
+            Console.WriteLine(dobások);
+            Console.ReadKey();
+        }
+    }
+}
