75bbc56b178d39c71b252648f61876884a4d58d0
korosa péter <97808318+Korosapeter@users.noreply.github.com>
2022-10-25 13:34:25 +0200

    Add files via upload

---
+++ számkitaláló/számkitaláló/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace számkitaláló
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int tipp, r, tippszám;
+            Console.WriteLine("Gondoltam egy számra");
+            r= random.Next(0,100);    
+            tipp=int.Parse(Console.ReadLine());
+            tippszám = 0;
+            while (tipp != r)
+            {
+                if (tipp > r)
+                {
+                    Console.WriteLine("Kissebb");
+                    tipp = int.Parse(Console.ReadLine());
+                    tippszám++;
+                }
+                if (tipp < r)
+                {
+                    Console.WriteLine("Nagyobb");
+                    tipp = int.Parse(Console.ReadLine());
+                    tippszám++;
+                }
+                if (tipp == r)
+                {
+                    Console.WriteLine("Eltaláltad");
+                    Console.WriteLine(tippszám);
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
