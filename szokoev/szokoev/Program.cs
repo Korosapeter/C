0d10826312dc3f4759fdec7a2318944551862d2d
korosa péter <97808318+Korosapeter@users.noreply.github.com>
2022-10-25 13:42:23 +0200

    Add files via upload

---
+++ szokoev/szokoev/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace szokoev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int szokoev;
+            Console.WriteLine("Adj meg egy évszámot");
+            szokoev = int.Parse(Console.ReadLine());
+            Console.WriteLine(szokoev);
+            if (szokoev % 4 == 0 & szokoev % 100 == 0) 
+            {
+                Console.WriteLine("Szőkőév");
+            }
+            else
+            { 
+            Console.WriteLine("Nem szökőév");
+                    }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
