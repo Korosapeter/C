c9133f22f4631be6cb640ce8084d4a0c22d38b53
korosa péter <97808318+Korosapeter@users.noreply.github.com>
2022-10-25 13:39:00 +0200

    Add files via upload

---
+++ pitagorasz/pitagorasz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pitagorasz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,c;
+            Console.WriteLine("Add meg az a oldalt:");
+            a=int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a b oldalt:");
+            b =int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a c oldalt:");
+            c =int.Parse(Console.ReadLine());
+            if (a + b <= c)
+            {
+                Console.WriteLine("A háromszög nem létezik");
+            }
+            else if (b + c <= a)
+            {
+                Console.WriteLine("A háromszög nem létezik");
+            }
+            else if (a + c <= b)
+            {
+                Console.WriteLine("A háromszög nem létezik");
+            }
+            else
+                Console.WriteLine("A háromszög létezik");
+            Console.ReadKey();
+        }
+    }
+}
