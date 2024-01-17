74531c682ae9bcb79d42599cb998f0fcb275a500
korosa péter <97808318+Korosapeter@users.noreply.github.com>
2024-01-17 09:50:05 +0100

    Add files via upload

---
+++ FutarWPF/FutarWPF/Models/Etel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutarWPF.Models
+{
+    internal class Etel
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public string Kategoria { get; set; }
+        public int ar { get; set; }
+        
+    }
+}
---
+++ FutarWPF/FutarWPF/Models/KiszallitasContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutarWPF.Models
+{
+    internal class KiszallitasContext : DbContext
+    {
+        public DbSet<Etel> Etel { get; set; }
+        public DbSet<futar> Futar { get; set; }
+        public DbSet<megrendeles> Megrendeles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //ConnectionString
+            string c = "server = localhost; userid = root; password =; database = Futa";
+
+            //OptionBulider: ConnectionString + Szerver verzió (manuálisan, vagy Autodetect)
+            optionsBuilder.UseMySql(c, ServerVersion.AutoDetect(c));
+
+
+        }
+    }
+}
